238f7c5203f75e7260657af5c67a8e4441d95df2
1110738MiguelFontes <1110738@isep.ipp.pt>
2013-03-13 16:53:14 +0000

    Commit inicial

---
+++ EAPLI/src/eapli/EAPLI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli;
+
+/**
+ *
+ * @author Miguel
+ */
+public class EAPLI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá");
+    }
+}
